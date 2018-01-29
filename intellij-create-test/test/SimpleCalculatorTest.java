d58166a588e94d503ada372d0882ab0efa0c2369
cybero <cybero@me.com>
2018-01-29 18:50:40 +0000

    Initial commit

---
+++ intellij-create-test/test/SimpleCalculatorTest.java
@@ -0,0 +1,35 @@
+package com.peter;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleCalculatorTest {
+
+    private SimpleCalculator calculator;
+
+    @Before
+    public void setUp() {
+        this.calculator = new SimpleCalculator();
+    }
+
+    @Test
+    public void testAdd() {
+        float n1 = 1;
+        float n2 = 2;
+        float expected = 3;
+        float actual;
+
+        actual = this.calculator.add(n1, n2);
+
+        assertEquals(expected, actual, 0.000);
+    }
+
+    @Test
+    public void setUp1() {
+    }
+
+    @Test
+    public void testAdd1() {
+    }
+}
